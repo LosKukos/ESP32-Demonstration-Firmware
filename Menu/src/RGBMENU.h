29c0f407562bef8eecddc633052f68b41e6a1df1
Jakub Hradil <166697451+LosKukos@users.noreply.github.com>
2026-04-30 16:43:48 +0200

    Přidání komentářů

--- Menu/src/RGBMENU.h
+++ Menu/src/RGBMENU.h
@@ -1,5 +1,4 @@
-#ifndef RGBMENU_H
-#define RGBMENU_H
+#pragma once
 
 #include <Arduino.h>
 #include <controls.h>
@@ -9,10 +8,15 @@ class RGBMenu {
 public:
     explicit RGBMenu(Controls& ctrl, Settings& settingsRef);
 
+    // Inicializace modulu
     void begin();
+
+    // Stav modulu
     bool shouldExit();
     bool isRunning();
     void requestStop();
+
+    // Webové rozhraní
     void registerWebRoutes();
 
 private:
@@ -28,22 +32,30 @@ private:
     Settings& settings;
     State state;
 
+    // Položky menu
     static constexpr uint8_t menuLength = 5;
     const char* menuItems[menuLength] = {"R", "G", "B", "Reset", "Konec"};
 
+    // FreeRTOS task modulu
     TaskHandle_t taskHandle = nullptr;
+    static void task(void* pvParameters);
+
+    // Sdílená data modulu
     SemaphoreHandle_t dataMutex = nullptr;
     bool webRoutesRegistered = false;
     bool running = false;
 
-    static void task(void* pvParameters);
-
+    // Zpracování vstupů
     void handleTouch();
+
+    // Vykreslení rozhraní
     void drawMenu(const State& snapshot);
     void drawSlider(int x, int y, int w, int h, int value, const char* label, bool selected);
+
+    // Ovládání LED diody
     void applyLedColor(uint8_t r, uint8_t g, uint8_t b);
+
+    // Práce se stavem modulu
     State copyState();
     void resetState(bool requestExit);
 };
-
-#endif

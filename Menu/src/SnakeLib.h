4a13dc7129afa1ace3cc841e9b5beb005c121e24
Jakub Hradil <166697451+LosKukos@users.noreply.github.com>
2026-04-30 16:48:31 +0200

    Přidání komentářů

--- Menu/src/SnakeLib.h
+++ Menu/src/SnakeLib.h
@@ -1,5 +1,4 @@
-#ifndef SNAKE_LIB_H
-#define SNAKE_LIB_H
+#pragma once
 
 #include <Wire.h>
 #include <controls.h>
@@ -11,12 +10,18 @@ class SnakeLib {
 public:
     SnakeLib(Controls& ctrl, Settings& settingsRef);
 
+    // Inicializace modulu
     void begin();
+
+    // Stav modulu
     bool shouldExit();
     bool isRunning();
     void requestStop() { _exit = true; }
 
+    // Webové rozhraní
     void registerWebRoutes();
+
+    // Nastavení obtížnosti
     void setDifficulty(int level);
 
 private:
@@ -49,8 +54,11 @@ private:
     Controls& controls;
     Settings& settings;
 
+    // Nastavení menu a obtížnosti
     static constexpr int MENU_ITEM_COUNT = 3;
     static constexpr int DIFFICULTY_COUNT = 4;
+
+    // Nastavení herní plochy
     static constexpr int CELL_SIZE = 4;
     static constexpr int GRID_MIN_X = 1;
     static constexpr int GRID_MIN_Y = 1;
@@ -59,6 +67,8 @@ private:
     static constexpr int GRID_WIDTH = GRID_MAX_X - GRID_MIN_X + 1;
     static constexpr int GRID_HEIGHT = GRID_MAX_Y - GRID_MIN_Y + 1;
     static constexpr int MAX_SNAKE_LENGTH = GRID_WIDTH * GRID_HEIGHT;
+
+    // Nastavení tabulky výsledků
     static constexpr int MAX_LEADERBOARD = 10;
     static constexpr const char* LEADERBOARD_FILE = "/leaderboard.json";
 
@@ -66,15 +76,21 @@ private:
     static const char* const DIFFICULTY_LEVELS[DIFFICULTY_COUNT];
     static const int DIFFICULTY_SPEEDS[DIFFICULTY_COUNT];
 
+    // FreeRTOS task modulu
     TaskHandle_t taskHandle = nullptr;
+    static void task(void *pvParameters);
+
+    // Sdílená data modulu
     SemaphoreHandle_t dataMutex = nullptr;
+    bool running = false;
 
+    // Stav hry
     GameState _gameState = MENU;
-
     int _menuIndex = 0;
     int _difficultyIndex = 1;
     int _gameSpeed = DIFFICULTY_SPEEDS[1];
 
+    // Herní data
     int _snakeLength = 2;
     int _dirX = 1;
     int _dirY = 0;
@@ -82,45 +98,50 @@ private:
     Point _fruit = {GRID_MIN_X, GRID_MIN_Y};
     int _fruitsEaten = 0;
 
+    // Časování hry a bzučáku
     unsigned long _lastMoveTime = 0;
     unsigned long _buzzerEndTime = 0;
     unsigned long _gameOverBeepUntil = 0;
 
     volatile bool _exit = false;
 
+    // Data pro ukládání výsledků
     PendingResult pendingResult = {0, 0, 0, false};
     LeaderboardEntry leaderboard[MAX_LEADERBOARD] = {};
     uint8_t leaderboardCount = 0;
 
-    static void task(void *pvParameters);
-    bool running = false;
-
+    // Herní logika
     void runGameLogic();
-    void drawMenu();
-    void drawSettings();
-    void drawGame();
-    void drawGameOver();
-
-    void handleButtonPress();
-    void handleTouchConfirm();
-    void updateDifficultyLED();
-    void updateBuzzer();
-
     void resetGame();
     void gameOver();
     void spawnFruit();
     bool isCellOccupied(int x, int y) const;
     int calculateWeightedScore(int fruits, int difficultyIndex) const;
     int currentChaosSpeed() const;
-    void invalidatePendingResult();
 
+    // Vykreslení obrazovek
+    void drawMenu();
+    void drawSettings();
+    void drawGame();
+    void drawGameOver();
+
+    // Zpracování vstupů
+    void handleButtonPress();
+    void handleTouchConfirm();
+
+    // Ovládání LED a bzučáku
+    void updateDifficultyLED();
+    void updateBuzzer();
+
+    // Práce s výsledky
+    void invalidatePendingResult();
     void loadLeaderboard();
     void saveLeaderboard();
     void clearLeaderboardInternal();
     bool insertLeaderboardEntry(const char* name);
     void sortLeaderboard();
+
+    // Uzamčení sdílených dat
     bool lockData(TickType_t ticks = portMAX_DELAY);
     void unlockData();
 };
-
-#endif

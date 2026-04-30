a2c0d23e7f67d14d3a87342efe705b8e95e95de2
Jakub Hradil <166697451+LosKukos@users.noreply.github.com>
2026-04-30 06:11:20 +0200

    Přidání komentářů

--- Menu/src/controls.h
+++ Menu/src/controls.h
@@ -6,16 +6,15 @@
 #include <Adafruit_SH110X.h>
 #include <Adafruit_NeoPixel.h>
 
-// Rozměry displeje
+// Rozměry OLED displeje
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-
 
 class Controls {
 public:
     Controls();
 
-    // Přiřazení hodnot k pinům
+    // Pinové přiřazení
     static constexpr int BUZZER_PIN = 2;
     static constexpr int LEFT_PIN = 16;
     static constexpr int RIGHT_PIN = 17;
@@ -28,39 +27,50 @@ public:
     static constexpr int NUM_LEDS = 1;
     static constexpr int LED_PIN = 27;
 
-    // Funkce pro inicializaci a ovládání ovládacích prvků
+    // Inicializace modulu
     void begin();
+
+    // Detekce dotyku
     bool fingerTouched();
     bool FingerTouchedFlag();
     void resetTouchState();
-    void mutexDisplay(std::function<void()> fn);
-    void mutexLed(std::function<void()> fn);
     void calibrateTouch();
 
+    // Uzamčení displeje a LED diody
+    void mutexDisplay(std::function<void()> fn);
+    void mutexLed(std::function<void()> fn);
+
+    // Stav tlačítek
     volatile bool leftPressed = false;
     volatile bool rightPressed = false;
 
+    // Objekty displeje a adresovatelné LED
     Adafruit_SH1106G display;
     Adafruit_NeoPixel strip;
 
 private:
-
-    // Proměnné pro detekci dotyku a debouncing
+    // Debounce tlačítek
     static constexpr unsigned long debounceTime = 250;
+
+    // Proměnné pro vyhodnocení dotyku
     int touch_baseline;
-    volatile unsigned long lastLeftTime;
-    volatile unsigned long lastRightTime;
     bool touchState = false;           
     bool prevTouchFlagState = false;    
     unsigned long lastTouchChangeTime = 0;
 
-    // Funkce pro zpracování stisků tlačítek a inicializaci displeje
+    // Časy posledního stisku tlačítek
+    volatile unsigned long lastLeftTime;
+    volatile unsigned long lastRightTime;
+
+    // Pomocné funkce modulu
     void initDisplay();
     void handleLeft();
     void handleRight();
 
+    // Mutexy pro displej a LED diodu
     SemaphoreHandle_t displayMutex;
     SemaphoreHandle_t ledMutex;
 
+    // Instance pro použití v přerušení
     static Controls* instance;
 };
